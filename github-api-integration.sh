6e013bf02147bd8959ef1c190686e0623ffb6304
utsxvrai <utsavrai2115@gmail.com>
2024-03-16 21:28:35 +0530

    Started new project

---
+++ github-api-integration.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+################################
+# Author: Utsav Rai
+# Version: v1
+#
+#
+#
+# This script will help users to communicate and retrieve information from GitHub
+# Usage:
+#   Please provide your github token and rest api to the script as input
+#
+################################
